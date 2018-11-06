0b5eccaaf88bfa9c444d826a5dcba47c0a83c19f
Roberto <sanchez.roberto0810@gmail.com>
2018-11-06 14:51:27 -0600

    Function que Actualiza el ultimo mensaje del chat general

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  console.log('It Works')
+});
+
+exports.updateLastMessage = functions.firestore.document('/chat_publico/chat_general/chat_msg/{msgId}')
+  .onCreate((snapshot, context) => {
+    return admin.firestore()
+      .collection('chat_publico')
+      .doc('chat_general')
+      .update({ last_message: snapshot.data() })
+  })
