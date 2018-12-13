efff1e7364e1c2449669e79e4dd840f1174f5d19
Roberto <sanchez.roberto0810@gmail.com>
2018-12-13 17:03:51 -0600

    funciona! xd

--- public/js/Components/Home/Chat/Message.js
+++ public/js/Components/Home/Chat/Message.js
@@ -1,11 +1,11 @@
 import { Firestore } from "../../../firebase";
 import { h } from 'preact'
 
-export default ({ message: { datetime, message, nickname, uID } }) => {
-  console.log(message)
+export default ({ message: { nickname, message, datetime } }) => {
+
   return (
     <div>
-      <div>{nickname || ''}</div>
+      <div>{nickname}</div>
       <div>{message}</div>
       <div>{datetime.toString()}</div>
     </div>
