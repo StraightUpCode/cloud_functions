5c7ff8acb86f8e21d5a29013ade90170bd201e20
Roberto <sanchez.roberto0810@gmail.com>
2018-12-15 14:27:36 -0600

    Re Organizacion de Archivos

--- public/js/Components/ChatRoom/ChatRoom.js
+++ public/js/Components/ChatRoom/ChatRoom.js
@@ -1,11 +1,11 @@
 import { h, Component } from 'preact'
-import Message from './Message';
+import Message from './Message/Message';
 
 export default class ChatRoom extends Component {
 
-  render({ chatRoom: { chatName, last_message } }, state) {
+  render({ chatRoom: { chatName, last_message }, ...props }, state) {
     return (
-      <div>
+      <div {...props}>
         <div>{chatName}</div>
         {!!last_message ? <Message message={last_message}></Message> : null}
       </div>)
